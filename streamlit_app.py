8e75940bb22de4d17a5ccca26f503760a71e3d13
AdamCapitalComplianceRisco <compliance@adamcapital.com.br>
2024-07-31 19:07:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,28 @@
 import streamlit as st
 import pandas as pd
 
-# Definir o DataFrame localmente (substitua isto pelo DataFrame real criado)
-df = pd.DataFrame({
-    'ProductClass': ['Cash', 'Cash', 'Cash', 'Cash', 'Cash', 'Currencies NDF', 'HUF/USD Futures - ICE', 'USD/CNH Futures - SGX', 'USD/CNH Futures - SGX', 'USD/CNH Futures - SGX'],
-    'Primeiro Aviso': ['']*10,
-    'Último Trade': ['']*10,
-    'Dias Úteis Para Liquidação': ['']*10,
-    'Entrega Física': ['']*10
-})
+# Defina o caminho do arquivo TXT
+file_path = r'Z:/Riscos/Planilhas/Atuais/Power BI/Bases Carteiras/AllTradingDesksVaRStress26Jul2024.txt'
+
+# Tente ler o arquivo e criar o DataFrame
+try:
+    # Carregar o arquivo TXT
+    df = pd.read_csv(file_path, delimiter='\t')  # Ajuste o delimitador conforme necessário
+
+    # Verifique se a coluna 'ProductClass' está presente e crie o DataFrame com dados reais
+    if 'ProductClass' in df.columns:
+        # Crie um novo DataFrame com a coluna 'ProductClass' e adicione colunas fictícias
+        df_selected = df[['ProductClass']].copy()
+        df_selected["Primeiro Aviso"] = ""
+        df_selected["Último Trade"] = ""
+        df_selected["Dias Úteis Para Liquidação"] = ""
+        df_selected["Entrega Física"] = ""
+    else:
+        df_selected = pd.DataFrame(columns=['ProductClass', 'Primeiro Aviso', 'Último Trade', 'Dias Úteis Para Liquidação', 'Entrega Física'])
+        st.error("Coluna 'ProductClass' não encontrada no arquivo TXT.")
+except Exception as e:
+    df_selected = pd.DataFrame(columns=['ProductClass', 'Primeiro Aviso', 'Último Trade', 'Dias Úteis Para Liquidação', 'Entrega Física'])
+    st.error(f"Ocorreu um erro ao ler o arquivo TXT: {e}")
 
 # Configurar a página do Streamlit
 st.set_page_config(page_title="Rolagem", page_icon="🎫")
@@ -18,4 +32,4 @@ st.title("Rolagem")
 st.write("Aqui será possível verificar as rolagens dos Ativos (Last Date Tradeble)")
 
 # Exibir o DataFrame no Streamlit
-st.dataframe(df, use_container_width=True, hide_index=True)
+st.dataframe(df_selected, use_container_width=True, hide_index=True)
