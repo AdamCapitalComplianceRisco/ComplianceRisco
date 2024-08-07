6e413216f55e6bc607a19d4bac89ea68e5ca1e70
AdamCapitalComplianceRisco <compliance@adamcapital.com.br>
2024-08-07 15:34:56 -0300

    Update risk_dashboard.py

--- risk_dashboard.py
+++ risk_dashboard.py
@@ -297,7 +297,7 @@ def anomaly_detection():
 
 #------------------------------------------------------------------------------------
 
-# Conexão com o banco de dados
+# Configuração do banco de dados
 engine = create_engine("mssql+pyodbc://sqladminadam:qpE3gEF2JF98e2PBg@adamcapitalsqldb.database.windows.net/AdamDB?driver=ODBC+Driver+17+for+SQL+Server")
 
 # Função para buscar dados do banco de dados
@@ -312,9 +312,6 @@ def pnl_dashboard():
     latest_date_query = "SELECT MAX(TRY_CONVERT(DATE, ValDate, 103)) AS LatestDate FROM AdamDB.DBO.Carteira"
     latest_date_result = fetch_data(latest_date_query)
     latest_date_str = latest_date_result['LatestDate'][0]
-
-    # Debug: Mostrar o valor de latest_date_str e seu tipo
-
 
     if pd.isna(latest_date_str):
         st.error('No data available in the database.')
@@ -355,8 +352,6 @@ def pnl_dashboard():
         books['RenamedBook'] = books['Book'].apply(rename_books)
         selected_books = st.multiselect('Select Books', books['RenamedBook'].unique(), default=books['RenamedBook'].unique())
 
-
-
         # Mapear os nomes renomeados de volta para os nomes originais dos livros
         selected_books_filtered = books[books['RenamedBook'].isin(selected_books)]
 
@@ -364,7 +359,6 @@ def pnl_dashboard():
         selected_books_filtered = selected_books_filtered[selected_books_filtered['Book'].str.contains('-SD|[-AF]|Mesa', na=False)]
 
         selected_books_original = selected_books_filtered['Book'].tolist()
-
 
         # Obter todos os dados para o intervalo de datas selecionado
         query = f"""
@@ -374,10 +368,8 @@ def pnl_dashboard():
         """
         params = tuple(selected_books_original) + (start_date_str, end_date_str)
 
-
         try:
             data = fetch_data(query, params)
-
         except Exception as e:
             st.error(f'Error executing query: {e}')
             return
@@ -395,6 +387,9 @@ def pnl_dashboard():
 
                 # Agrupando os dados por Product e Book e somando o PNL
                 grouped_data = filtered_data.groupby(['Product', 'Book'])['PL'].sum().reset_index()
+
+                # Ordenar o DataFrame pelo PNL em ordem decrescente
+                grouped_data = grouped_data.sort_values(by='PL', ascending=False)
 
                 # Gráfico de barras de PNL por Produto
                 fig = px.bar(grouped_data, x='Product', y='PL', color='Book', barmode='group',
@@ -419,7 +414,6 @@ def pnl_dashboard():
                 st.error('No data available for the selected books.')
         else:
             st.error('No data available for the selected date range.')
-
     except ValueError as e:
         st.error(f'Error parsing date: {latest_date_str}. Error: {e}')
     except Exception as e:
